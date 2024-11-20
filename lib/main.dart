1c16d39920a8072d240664084051338a1d283c77
berkaytikenoglu <berkaytikenoglu@gmail.com>
2024-11-20 04:43:08 +0300

    webe uyumlu hale getirldi # etiketi kaldırıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:aramizdakioyuncucom/app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(
     const App(),
   );
